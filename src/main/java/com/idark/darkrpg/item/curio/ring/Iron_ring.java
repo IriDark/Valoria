6519bde30d593172dbf922f84b64e09fb62f3d3f
iDark <69202490+iDarkMage@users.noreply.github.com>
2022-11-07 19:32:57 +0200

    Update Iron_ring.java

--- src/main/java/com/idark/darkrpg/item/curio/ring/Iron_ring.java
+++ src/main/java/com/idark/darkrpg/item/curio/ring/Iron_ring.java
@@ -18,6 +18,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.SoundEvents;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.CuriosCapability;
 import top.theillusivec4.curios.api.SlotContext;
@@ -26,30 +27,47 @@ import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraftforge.common.ForgeMod;
 import javax.annotation.Nonnull;
 import java.util.UUID;
 
 
 public class Iron_ring extends Item implements ICurioItem {
+	public static final String MOD_ID = "darkrpg";
 
     public Iron_ring() {
-        super(new Properties().rarity(Rarity.RARE).group(ModItemGroup.DARKRPG_GROUP)); 
+        super(
+        new Properties()
+        .rarity(Rarity.EPIC)
+        .group(ModItemGroup.DARKRPG_GROUP)); 
     }
 
     @Override
-    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext, UUID uuid, ItemStack stack) {
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(SlotContext slotContext,
+        UUID uuid, ItemStack stack) {
         Multimap<Attribute, AttributeModifier> atts = LinkedHashMultimap.create();
-	atts.put(Attributes.MAX_HEALTH, new AttributeModifier(uuid, "bonus", 10.0, AttributeModifier.Operation.ADDITION));
+        atts.put(Attributes.ARMOR, new AttributeModifier(uuid, DarkRPG.MOD_ID + ":armor_bonus", 1, AttributeModifier.Operation.ADDITION));
         return atts;
+	}
+	
+    @Override
+    public boolean canEquipFromUse(SlotContext slot, ItemStack stack) {
+        return true;
     }
-  
-      @Override
+	
+    @Nonnull
+    @Override
+    public ICurio.SoundInfo getEquipSound(SlotContext slotContext, ItemStack stack) {
+        return new ICurio.SoundInfo(SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 1.0f, 1.0f);
+    }
+	
+    @Override
     public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
         PlayerEntity player = (PlayerEntity) livingEntity;
 
-                if(random.nextFloat() > 0.6f) {
-                    stack.damageItem(1, player, p -> CuriosApi.getCuriosHelper().onBrokenCurio(
-                            SlotTypePreset.RING.getIdentifier(), index, p));
+        if(random.nextFloat() > 0.6f) {
+        stack.damageItem(1, player, p -> CuriosApi.getCuriosHelper().onBrokenCurio(
+        SlotTypePreset.RING.getIdentifier(), index, p));
 	    }
 	}
 }
