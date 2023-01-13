2b00cb5f258ca2994494cea6cd64231c86154c15
iDark <69202490+iDarkMage@users.noreply.github.com>
2023-01-13 10:43:50 +0200

    Update Rune_of_pyro.java

--- src/main/java/com/idark/darkrpg/item/curio/charm/Rune_of_pyro.java
+++ src/main/java/com/idark/darkrpg/item/curio/charm/Rune_of_pyro.java
@@ -7,6 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.potion.Effect;
@@ -27,24 +28,38 @@ import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Random;
 import java.util.List;
 
 
-public class Rune_of_pyro extends Item implements ICurioItem {
+public class CurioPyro extends Item implements ICurioItem {
 
-    public Rune_of_pyro() {
-        super(
-		new Properties()
-		.rarity(Rarity.EPIC)
-		.group(ModItemGroup.DARKRPG_GROUP));
+    public CurioPyro(Properties properties) {
+        super(properties);
 	}
 	
-    public void wearableTick(LivingEntity wearer) {
-    wearer.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 100, 0));
-	}
+    @Override
+    public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
+        PlayerEntity player = (PlayerEntity) livingEntity;
+
+        if(!player.world.isRemote()) {
+            boolean hasPlayerFireResistance =
+                    !Objects.equals(player.getActivePotionEffect(Effects.FIRE_RESISTANCE), null);
+
+            if(!hasPlayerFireResistance) {
+                player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 200));
+
+                if(random.nextFloat() > 0.6f) {
+                    stack.damageItem(1, player, p -> CuriosApi.getCuriosHelper().onBrokenCurio(
+                            SlotTypePreset.CHARM.getIdentifier(), index, p));
+                }
+            }
+        }
+
+        ICurioItem.super.curioTick(identifier, index, livingEntity, stack);
+    }
 	
-        //dont working
 	@Override
 	public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flags) {
 		super.addInformation(stack, world, tooltip, flags);
