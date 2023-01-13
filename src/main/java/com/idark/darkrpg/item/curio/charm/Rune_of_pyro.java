2fa4dd074ecbbd0a69b24b9ee4f3d779bf6c9deb
iDark <69202490+iDarkMage@users.noreply.github.com>
2023-01-13 08:21:53 +0200

    Updating repo

--- src/main/java/com/idark/darkrpg/item/curio/charm/Rune_of_pyro.java
+++ src/main/java/com/idark/darkrpg/item/curio/charm/Rune_of_pyro.java
@@ -3,18 +3,13 @@ package com.idark.darkrpg.item.curio.charm;
 import com.idark.darkrpg.DarkRPG;
 import com.idark.darkrpg.item.ModItems;
 import com.idark.darkrpg.item.ModItemGroup;
-import com.google.common.collect.LinkedHashMultimap;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
-import net.minecraft.item.Rarity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Rarity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.SoundEvents;
+import net.minecraft.potion.Effect;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.world.World;
 import net.minecraft.client.util.ITooltipFlag;
@@ -23,20 +18,18 @@ import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.SlotTypePreset;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.UUID;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.List;
+
 
 public class Rune_of_pyro extends Item implements ICurioItem {
 
@@ -47,30 +40,14 @@ public class Rune_of_pyro extends Item implements ICurioItem {
 		.group(ModItemGroup.DARKRPG_GROUP));
 	}
 	
-    @Override
-    public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
-        PlayerEntity player = (PlayerEntity) livingEntity;
-
-        if(!player.world.isRemote()) {
-            boolean hasPlayerFireResistance =
-                    !Objects.equals(player.getActivePotionEffect(Effects.FIRE_RESISTANCE), null);
-
-            if(!hasPlayerFireResistance) {
-                player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 200));
-
-                if(random.nextFloat() > 0.6f) {
-                    stack.damageItem(1, player, p -> CuriosApi.getCuriosHelper().onBrokenCurio(
-                            SlotTypePreset.CHARM.getIdentifier(), index, p));
-                }
-            }
-        }
-		
-        ICurioItem.super.curioTick(identifier, index, livingEntity, stack);
-    }
+    public void wearableTick(LivingEntity wearer) {
+    wearer.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 100, 0));
+	}
 	
-	@OnlyIn(Dist.CLIENT)
+        //dont working
 	@Override
-	public void addInformation(ItemStack stack, World world, List<ITextComponent> list, ITooltipFlag flag) {
-		list.add(new TranslationTextComponent("tooltip.darkrpg.pyro").mergeStyle(TextFormatting.RED));
+	public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flags) {
+		super.addInformation(stack, world, tooltip, flags);
+		tooltip.add(new TranslationTextComponent("tooltip.darkrpg.pyro").mergeStyle(TextFormatting.GRAY));
 	}
 }
