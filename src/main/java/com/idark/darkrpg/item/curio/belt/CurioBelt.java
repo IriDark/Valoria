55c05ca36376ebbf22b242b7d984a57f8ae46be6
iDark <69202490+iDarkMage@users.noreply.github.com>
2023-03-07 17:13:27 +0200

    Update CurioBelt.java

--- src/main/java/com/idark/darkrpg/item/curio/belt/CurioBelt.java
+++ src/main/java/com/idark/darkrpg/item/curio/belt/CurioBelt.java
@@ -1,13 +1,19 @@
-package com.idark.darkrpg.item.curio.charm;
+package com.idark.darkrpg.item.curio.belt;
 
 import com.idark.darkrpg.DarkRPG;
 import com.idark.darkrpg.item.ModItems;
 import com.idark.darkrpg.item.ModItemGroup;
+import com.idark.darkrpg.item.curio.*;
+import com.idark.darkrpg.client.render.curio.model.*;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.item.*;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.ai.attributes.Attribute;
@@ -20,6 +26,7 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import top.theillusivec4.curios.api.CuriosCapability;
 import top.theillusivec4.curios.api.CuriosApi;
@@ -35,7 +42,9 @@ import java.util.Random;
 import java.util.List;
 
 
-public class CurioBelt extends Item implements ICurioItem {
+public class CurioBelt extends RPGCurioItem {
+	
+   private static final ResourceLocation BELT_TEXTURE = new ResourceLocation(DarkRPG.MOD_ID, "textures/entity/leather_belt.png");
 
     public CurioBelt(Properties properties) {
         super(properties);
@@ -55,4 +64,18 @@ public class CurioBelt extends Item implements ICurioItem {
 	super.addInformation(stack, world, tooltip, flags);
 	tooltip.add(new TranslationTextComponent("tooltip.darkrpg.belt").mergeStyle(TextFormatting.GRAY));
 	}
+	
+	@Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack) {
+	BeltModel model = new BeltModel();
+	ICurio.RenderHelper.translateIfSneaking(matrixStack, livingEntity);
+    ICurio.RenderHelper.rotateIfSneaking(matrixStack, livingEntity);
+    IVertexBuilder vertexBuilder = ItemRenderer.getBuffer(renderTypeBuffer, model.getRenderType(BELT_TEXTURE), false, stack.hasEffect());
+    model.render(matrixStack, vertexBuilder, light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+	}
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
+        return true;
+    }
 }
