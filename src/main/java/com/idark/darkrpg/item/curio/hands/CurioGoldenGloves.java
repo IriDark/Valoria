a7f504aca8d7975217684c334acfbc1428e9e130
iDark <69202490+iDarkMage@users.noreply.github.com>
2023-03-07 17:14:47 +0200

    Update CurioGoldenGloves.java

--- src/main/java/com/idark/darkrpg/item/curio/hands/CurioGoldenGloves.java
+++ src/main/java/com/idark/darkrpg/item/curio/hands/CurioGoldenGloves.java
@@ -3,27 +3,30 @@ package com.idark.darkrpg.item.curio.hands;
 import com.idark.darkrpg.DarkRPG;
 import com.idark.darkrpg.item.ModItems;
 import com.idark.darkrpg.item.ModItemGroup;
+import com.idark.darkrpg.item.curio.*;
+import com.idark.darkrpg.client.render.curio.model.*;
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Rarity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.attributes.Attribute;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.SoundEvents;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.CuriosCapability;
 import top.theillusivec4.curios.api.SlotContext;
@@ -39,8 +42,10 @@ import javax.annotation.Nonnull;
 import java.util.UUID;
 import java.util.List;
 
-public class CurioGoldenGloves extends Item implements ICurioItem {
+public class CurioGoldenGloves extends RPGCurioItem {
 	
+   private static final ResourceLocation GLOVES_TEXTURE = new ResourceLocation(DarkRPG.MOD_ID, "textures/entity/golden_gloves.png");
+
    public CurioGoldenGloves(Properties properties) {
         super(properties);
 	}
@@ -65,9 +70,25 @@ public class CurioGoldenGloves extends Item implements ICurioItem {
 	    }
 	}
 	
-		@Override
+	@Override
 	public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flags) {
 		super.addInformation(stack, world, tooltip, flags);
 		tooltip.add(new TranslationTextComponent("tooltip.darkrpg.golden").mergeStyle(TextFormatting.GRAY));
 	}
+	
+    @Override
+    public void render(String identifier, int index, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, LivingEntity livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, ItemStack stack) {
+        super.render(identifier, index, matrixStack, renderTypeBuffer, light, livingEntity, limbSwing, limbSwingAmount, partialTicks, ageInTicks, netHeadYaw, headPitch, stack);
+        HandsModel model = new HandsModel();
+        model.setLivingAnimations(livingEntity, limbSwing, limbSwingAmount, partialTicks);
+        model.setRotationAngles(livingEntity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+        ICurio.RenderHelper.followBodyRotations(livingEntity, model);
+        IVertexBuilder vertexBuilder = ItemRenderer.getBuffer(renderTypeBuffer, model.getRenderType(GLOVES_TEXTURE), false, stack.hasEffect());
+        model.render(matrixStack, vertexBuilder, light, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+    }
+
+    @Override
+    public boolean canRender(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
+        return true;
+    }
 }
