2eee73e74c75c35574162a66e94485c842d9cc1e
iDark <69202490+iDarkMage@users.noreply.github.com>
2023-03-16 07:46:31 +0200

    mixins

---
+++ src/main/java/com/idark/darkrpg/mixin/MixinCooldownTracker.java
@@ -0,0 +1,27 @@
+package com.idark.darkrpg.mixin;
+
+import com.idark.darkrpg.item.CooldownHandler;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.util.ServerCooldownTracker;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin({ServerCooldownTracker.class})
+public class MixinCooldownTracker {
+
+    @Shadow
+    @Final
+    private ServerPlayerEntity player;
+
+
+    @Inject(method = "notifyOnRemove",at = @At("HEAD"))
+    public void onRemove(Item itemIn, CallbackInfo ci) {
+        CooldownHandler.onCooldownEnd(player,itemIn);
+    }
+
+}
