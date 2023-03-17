73b5013ea96ab8fe5b32a75794aec4bbd6e9ab59
iDark <69202490+iDarkMage@users.noreply.github.com>
2023-03-17 15:54:02 +0200

    фикс гелия а то сегодня я выходной плохой выходит

--- src/main/java/com/idark/darkrpg/effect/TipsyEffect.java
+++ src/main/java/com/idark/darkrpg/effect/TipsyEffect.java
@@ -11,7 +11,6 @@ public class TipsyEffect extends Effect {
     public TipsyEffect() {
         super(EffectType.HARMFUL, 0x6CCE31);
 		addAttributesModifier(Attributes.MOVEMENT_SPEED, "1107DE5E-7AE8-2030-840A-21B21F160890", (double)-0.5F, AttributeModifier.Operation.MULTIPLY_TOTAL);
-   		addAttributesModifier(Attributes.KNOCKBACK_RESISTANCE, "B500EFB0-44AA-4F2B-8310-BEFBF96D540A ", (double)0.3F, AttributeModifier.Operation.MULTIPLY_TOTAL);
 		addAttributesModifier(Attributes.ARMOR, "74841448-7BD1-4C3F-924D-EED3F7A6E439", (double)-0.5F, AttributeModifier.Operation.MULTIPLY_TOTAL);
 		addAttributesModifier(Attributes.ATTACK_DAMAGE, "22653B89-116E-49DC-9B6B-9971489B5BE5", (double)0.5F, AttributeModifier.Operation.MULTIPLY_TOTAL);
        	addAttributesModifier(Attributes.ATTACK_SPEED, "55FCED67-E92A-486E-9800-B47F202C4386", (double)0.2F, AttributeModifier.Operation.MULTIPLY_TOTAL);
