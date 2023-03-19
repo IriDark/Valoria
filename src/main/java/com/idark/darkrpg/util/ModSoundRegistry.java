a23633156d42bfbcfe688fbd69228340dad806ce
iDark <69202490+iDarkMage@users.noreply.github.com>
2023-03-19 17:09:01 +0200

    cum eruption

--- src/main/java/com/idark/darkrpg/util/ModSoundRegistry.java
+++ src/main/java/com/idark/darkrpg/util/ModSoundRegistry.java
@@ -18,6 +18,7 @@ public class ModSoundRegistry {
     public static final RegistryObject<SoundEvent> SWIFTSLICE = registerSound("item.swiftslice.use");
 	public static final RegistryObject<SoundEvent> RECHARGE = registerSound("item.recharge.use");
 	public static final RegistryObject<SoundEvent> BLAZECHARGE = registerSound("item.blazecharge.use");
+	public static final RegistryObject<SoundEvent> ERUPTION = registerSound("item.eruption.use");
 	public static final RegistryObject<SoundEvent> PIERCE1 = registerSound("item.pierce1.ambient");
 	public static final RegistryObject<SoundEvent> PIERCE2 = registerSound("item.pierce2.ambient");
 	public static final RegistryObject<SoundEvent> PIERCE3 = registerSound("item.pierce3.ambient");
