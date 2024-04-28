ccfec87b9a7d1d13cd6b1e5b23e459cd64a8d24b
IriDark <idarkmage228@gmail.com>
2024-04-28 14:42:32 +0300

    trades fix

--- src/main/java/com/idark/valoria/core/event/VillagerTrades.java
+++ src/main/java/com/idark/valoria/core/event/VillagerTrades.java
@@ -24,21 +24,21 @@ public class VillagerTrades {
         public static void addTrades(VillagerTradesEvent event) {
             Int2ObjectMap<List<net.minecraft.world.entity.npc.VillagerTrades.ItemListing>> trades = event.getTrades();
             if (event.getType() == VillagerProfession.TOOLSMITH) {
-                trades.get(1).add(itemPurchase(6, ItemsRegistry.IRON_RING.get(), 1, 4, 2));
-                trades.get(2).add(itemPurchase(16, ItemsRegistry.IRON_GLOVES.get(), 1, 1, 12));
-                trades.get(1).add(itemPurchase(4, ItemsRegistry.IRON_CHAIN.get(), 1, 2, 6));
-                trades.get(3).add(itemPurchase(18, ItemsRegistry.IRON_RING_EMERALD.get(), 1, 1, 12));
+                trades.get(1).add(itemSell(6, ItemsRegistry.IRON_RING.get(), 1, 4));
+                trades.get(2).add(itemSell(16, ItemsRegistry.IRON_GLOVES.get(), 1, 1));
+                trades.get(1).add(itemSell(4, ItemsRegistry.IRON_CHAIN.get(), 1, 2));
+                trades.get(3).add(itemSell(18, ItemsRegistry.IRON_RING_EMERALD.get(), 1, 1));
             }
 
             if (event.getType() == VillagerProfession.WEAPONSMITH) {
-                trades.get(2).add(itemPurchase(14, ItemsRegistry.IRON_KATANA.get(), 1, 1, 12));
-                trades.get(1).add(itemPurchase(6, ItemsRegistry.IRON_RAPIER.get(), 1, 1, 6));
-                trades.get(1).add(itemPurchase(2, ItemsRegistry.CLUB.get(), 1, 2, 6));
-                trades.get(2).add(itemPurchase(22, ItemsRegistry.IRON_SCYTHE.get(), 1, 1, 16));
+                trades.get(2).add(itemSell(14, ItemsRegistry.IRON_KATANA.get(), 1, 1));
+                trades.get(1).add(itemSell(6, ItemsRegistry.IRON_RAPIER.get(), 1, 1));
+                trades.get(1).add(itemSell(2, ItemsRegistry.CLUB.get(), 1, 2));
+                trades.get(2).add(itemSell(22, ItemsRegistry.IRON_SCYTHE.get(), 1, 1));
             }
 
             if (event.getType() == VillagerProfession.LIBRARIAN) {
-                trades.get(1).add(itemPurchase(4, ItemsRegistry.LEXICON.get(), 1, 12, 5));
+                trades.get(1).add(itemSell(4, ItemsRegistry.LEXICON.get(), 1, 12));
             }
         }
 
@@ -47,16 +47,22 @@ public class VillagerTrades {
             List<net.minecraft.world.entity.npc.VillagerTrades.ItemListing> trades = event.getGenericTrades();
             System.out.print(trades.get(1));
             trades.add(itemSell(4, ItemsRegistry.IRON_RING.get(), 1, 12));
-            trades.add(itemPurchase(6, ItemsRegistry.KARUSAKAN_ROOT.get(), 1, 2, 12));
-            trades.add(itemPurchase(6, ItemsRegistry.GAIB_ROOT.get(), 1, 2, 12));
-            trades.add(itemPurchase(4, ItemsRegistry.ALOE_BANDAGE.get(), 2, 4, 6));
-            trades.add(itemPurchase(4, ItemsRegistry.ALOE_PIECE.get(), 1, 2, 12));
+            trades.add(itemSell(6, ItemsRegistry.KARUSAKAN_ROOT.get(), 1, 2));
+            trades.add(itemSell(6, ItemsRegistry.GAIB_ROOT.get(), 1, 2));
+            trades.add(itemSell(4, ItemsRegistry.ALOE_BANDAGE.get(), 2, 4));
+            trades.add(itemPurchase(4, ItemsRegistry.ALOE_PIECE.get(), 1, 2, 2));
         }
 
+        /**
+         * an Item that villager purchases
+         */
         public static BasicItemListing itemPurchase(int pEmeralds, ItemLike item, int count, int maxTrades, int xp) {
             return new BasicItemListing(new ItemStack(item, count), new ItemStack(Items.EMERALD, pEmeralds), maxTrades, xp, 0.05F);
         }
 
+        /**
+         * an Item that villager sells
+         */
         public static BasicItemListing itemSell(int pEmeralds, ItemLike item, int maxTrades, int xp) {
             return new BasicItemListing(pEmeralds, new ItemStack(item), maxTrades, xp, 0.05F);
         }
