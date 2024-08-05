9db72f0b1b49674f053405788b3afb1df2f919f0
IriDark <idarkmage228@gmail.com>
2024-08-05 16:32:29 +0300

    nuh uh

--- src/main/java/com/idark/valoria/registries/block/types/ManipulatorBlock.java
+++ src/main/java/com/idark/valoria/registries/block/types/ManipulatorBlock.java
@@ -40,7 +40,7 @@ public class ManipulatorBlock extends Block implements EntityBlock{
         VoxelShape shape = Shapes.empty();
         shape = Shapes.join(shape, Shapes.box(0.0625, 0, 0.0625, 0.9375, 0.1875, 0.9375), BooleanOp.OR);
         shape = Shapes.join(shape, Shapes.box(0.25, 0.1875, 0.25, 0.75, 0.3125, 0.75), BooleanOp.OR);
-        shape = Shapes.join(shape, Shapes.box(0, 0.3125, 0, 1, 0.5, 1), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0, 0.3125, 0, 1, 0.56, 1), BooleanOp.OR);
         shape = Shapes.join(shape, Shapes.box(0.6875, 0.5, 0.6875, 1, 1, 1), BooleanOp.OR);
         shape = Shapes.join(shape, Shapes.box(0, 0.5, 0.6875, 0.3125, 1, 1), BooleanOp.OR);
         shape = Shapes.join(shape, Shapes.box(0, 0.5, 0, 0.3125, 1, 0.3125), BooleanOp.OR);
