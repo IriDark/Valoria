4d825d5e54123a94654573427715a906533d2824
IriDark <idarkmage228@gmail.com>
2024-10-29 16:57:39 +0200

    glitchy

--- src/main/java/com/idark/valoria/client/render/entity/TrollRenderer.java
+++ src/main/java/com/idark/valoria/client/render/entity/TrollRenderer.java
@@ -4,18 +4,40 @@ import com.idark.valoria.*;
 import com.idark.valoria.client.model.entity.*;
 import com.idark.valoria.client.render.layers.*;
 import com.idark.valoria.registries.entity.living.*;
+import com.mojang.blaze3d.vertex.*;
+import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.entity.*;
 import net.minecraft.resources.*;
+import net.minecraft.util.*;
+import net.minecraft.world.phys.*;
 
 public class TrollRenderer extends MobRenderer<Troll, TrollModel<Troll>> {
     protected ResourceLocation texture;
-
+    private final boolean corrupted;
     public TrollRenderer(EntityRendererProvider.Context context, boolean corrupted) {
         super(context, new TrollModel<>(TrollModel.createBodyLayer().bakeRoot()), 0.8F);
+        this.corrupted = corrupted;
         this.texture = new ResourceLocation(Valoria.ID, corrupted ? "textures/entity/corrupted_troll.png" : "textures/entity/troll.png");
         this.addLayer(new LuminescentLayer.Builder<>(this)
-        .setTexture(new ResourceLocation(Valoria.ID, corrupted ? "textures/entity/corrupted_troll_eyes.png" : "textures/entity/troll_eyes_layer.png"))
-        .build());
+            .setTexture(new ResourceLocation(Valoria.ID, corrupted ? "textures/entity/corrupted_troll_eyes.png" : "textures/entity/troll_eyes_layer.png"))
+            .build()
+        );
+    }
+
+    public void render(Troll pEntity, float pEntityYaw, float pPartialTicks, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight) {
+        if (corrupted) {
+            Vec3[] avec3 = pEntity.getOffsets();
+            float f = this.getBob(pEntity, pPartialTicks);
+            for(int i = 0; i < avec3.length; ++i) {
+                pPoseStack.pushPose();
+                pPoseStack.translate(avec3[i].x + (double)Mth.cos((float)i + f * 0.5F) * 0.015D, avec3[i].y + (double)Mth.cos((float)i + f * 0.5F) * 0.0125D, avec3[i].z + (double)Mth.cos((float)i + f * 0.7F) * 0.015D);
+                super.render(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, pPackedLight);
+                pPoseStack.popPose();
+            }
+
+        } else {
+            super.render(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, pPackedLight);
+        }
     }
 
     @Override
--- src/main/java/com/idark/valoria/registries/entity/living/Troll.java
+++ src/main/java/com/idark/valoria/registries/entity/living/Troll.java
@@ -14,19 +14,26 @@ import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.entity.player.*;
 import net.minecraft.world.level.*;
 import net.minecraft.world.level.pathfinder.*;
+import net.minecraft.world.phys.*;
 
 import javax.annotation.*;
 
 public class Troll extends Monster {
     public final AnimationState idleAnimationState = new AnimationState();
     private int idleAnimationTimeout = 0;
-
+    public final Vec3[][] clientSideIllusionOffsets;
     public Troll(EntityType<? extends Monster> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
         this.xpReward = 3;
         this.setPathfindingMalus(BlockPathTypes.LAVA, 2.0F);
         this.setPathfindingMalus(BlockPathTypes.DAMAGE_OTHER, 4.0F);
         this.setPathfindingMalus(BlockPathTypes.DAMAGE_CAUTIOUS, 4.0F);
+        this.clientSideIllusionOffsets = new Vec3[2][4];
+
+        for(int i = 0; i < 4; ++i) {
+            this.clientSideIllusionOffsets[0][i] = Vec3.ZERO;
+            this.clientSideIllusionOffsets[1][i] = Vec3.ZERO;
+        }
     }
 
     @Override
@@ -35,6 +42,10 @@ public class Troll extends Monster {
         if (this.level().isClientSide()) {
             setupAnimationStates();
         }
+    }
+
+    public Vec3[] getOffsets() {
+        return this.clientSideIllusionOffsets[1];
     }
 
     private void setupAnimationStates() {
@@ -72,13 +83,13 @@ public class Troll extends Monster {
         super.registerGoals();
         // attack
         this.targetSelector.addGoal(0, new TrollAttackGoal(this, 1, false));
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        this.targetSelector.addGoal(0, new NearestAttackableTargetGoal<>(this, Player.class, true));
 
         // ai
         this.goalSelector.addGoal(1, new RestrictSunGoal(this));
-        this.goalSelector.addGoal(3, new LookAtPlayerGoal(this, Player.class, 8.0F));
-        this.goalSelector.addGoal(3, new RandomLookAroundGoal(this));
-        this.goalSelector.addGoal(4, new WaterAvoidingRandomStrollGoal(this, 0.8D));
+        this.goalSelector.addGoal(1, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(2, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 0.8D));
     }
 
     @Nullable
