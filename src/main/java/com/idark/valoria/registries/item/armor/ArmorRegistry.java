f84ac32b4ff45b9822a96e6a21daefad66cab3e3
IriDark <idarkmage228@gmail.com>
2025-03-02 16:05:27 +0200

    changes

--- src/main/java/com/idark/valoria/registries/item/armor/ArmorRegistry.java
+++ src/main/java/com/idark/valoria/registries/item/armor/ArmorRegistry.java
@@ -72,13 +72,13 @@ public class ArmorRegistry implements ArmorMaterial{
     // Netherite - 16%
     public static final ArmorRegistry MARSH = new ArmorRegistry.Builder("marsh").protection(20).mul(58).enchantValue(12).ingredient(() -> Ingredient.of(ItemsRegistry.marshCloth.get())).build();
     public static final ArmorRegistry SAMURAI = new ArmorRegistry.Builder("samurai").protection(22).mul(55).enchantValue(16).knockbackRes(0.15f).ingredient(() -> Ingredient.of(ItemsRegistry.ancientIngot.get())).build();
-    public static final ArmorRegistry COBALT = new ArmorRegistry.Builder("cobalt").protection(24).mul(46).enchantValue(18).knockbackRes(0.05f).ingredient(() -> Ingredient.of(ItemsRegistry.cobaltIngot.get())).build();
-    public static final ArmorRegistry ETHEREAL = new ArmorRegistry.Builder("ethereal").protection(26).mul(60).enchantValue(18).knockbackRes(0.1f).ingredient(() -> Ingredient.of(ItemsRegistry.etherealShard.get())).build();
-    public static final ArmorRegistry NATURE = new ArmorRegistry.Builder("nature").protection(28).mul(66).enchantValue(16).knockbackRes(0.10f).ingredient(() -> Ingredient.of(ItemsRegistry.natureIngot.get())).build();
-    public static final ArmorRegistry DEPTH = new ArmorRegistry.Builder("depth").protection(30).mul(72).enchantValue(16).knockbackRes(0.10f).ingredient(() -> Ingredient.of(ItemsRegistry.aquariusIngot.get())).build();
-    public static final ArmorRegistry INFERNAL = new ArmorRegistry.Builder("infernal").protection(32).mul(76).enchantValue(14).knockbackRes(0.12f).ingredient(() -> Ingredient.of(ItemsRegistry.infernalIngot.get())).build();
-    public static final ArmorRegistry SPIDER = new ArmorRegistry.Builder("spider").protection(35).mul(68).enchantValue(14).knockbackRes(0.10f).ingredient(() -> Ingredient.of(ItemsRegistry.spiderFang.get())).build();
-    public static final ArmorRegistry VOID = new ArmorRegistry.Builder("awakened_void").protection(40).mul(76).enchantValue(10).knockbackRes(0.15f).ingredient(() -> Ingredient.of(ItemsRegistry.voidIngot.get())).build();
+    public static final ArmorRegistry COBALT = new ArmorRegistry.Builder("cobalt").protection(25).mul(46).enchantValue(18).knockbackRes(0.05f).ingredient(() -> Ingredient.of(ItemsRegistry.cobaltIngot.get())).build();
+    public static final ArmorRegistry ETHEREAL = new ArmorRegistry.Builder("ethereal").protection(28).mul(60).enchantValue(18).knockbackRes(0.1f).ingredient(() -> Ingredient.of(ItemsRegistry.etherealShard.get())).build();
+    public static final ArmorRegistry NATURE = new ArmorRegistry.Builder("nature").protection(30).mul(66).enchantValue(16).knockbackRes(0.10f).ingredient(() -> Ingredient.of(ItemsRegistry.natureIngot.get())).build();
+    public static final ArmorRegistry DEPTH = new ArmorRegistry.Builder("depth").protection(32).mul(72).enchantValue(16).knockbackRes(0.10f).ingredient(() -> Ingredient.of(ItemsRegistry.aquariusIngot.get())).build();
+    public static final ArmorRegistry INFERNAL = new ArmorRegistry.Builder("infernal").protection(35).mul(76).enchantValue(14).knockbackRes(0.12f).ingredient(() -> Ingredient.of(ItemsRegistry.infernalIngot.get())).build();
+    public static final ArmorRegistry SPIDER = new ArmorRegistry.Builder("spider").protection(40).mul(68).enchantValue(14).knockbackRes(0.10f).ingredient(() -> Ingredient.of(ItemsRegistry.spiderFang.get())).build();
+    public static final ArmorRegistry VOID = new ArmorRegistry.Builder("awakened_void").protection(45).mul(76).enchantValue(10).knockbackRes(0.15f).ingredient(() -> Ingredient.of(ItemsRegistry.voidIngot.get())).build();
     public static final ArmorRegistry PHANTASM = new ArmorRegistry.Builder("phantasm").protection(50).mul(82).enchantValue(12).knockbackRes(0.25f).ingredient(() -> Ingredient.of(ItemsRegistry.illusionStone.get())).build();
 
     public static class Builder extends pro.komaru.tridot.registry.item.builders.AbstractArmorBuilder<ArmorRegistry>{
