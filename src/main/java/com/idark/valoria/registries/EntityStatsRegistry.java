88daeeebe2fab7462d91c632e8c7a7dfcab37635
IriDark <idarkmage228@gmail.com>
2025-03-24 23:33:06 +0200

    balance

--- src/main/java/com/idark/valoria/registries/EntityStatsRegistry.java
+++ src/main/java/com/idark/valoria/registries/EntityStatsRegistry.java
@@ -29,9 +29,9 @@ public class EntityStatsRegistry{
     public static AttributeSupplier TROLL = register(30, 8).build();
     public static AttributeSupplier DRAUGR = register(30, 5).add(Attributes.ARMOR, 5).add(Attributes.ARMOR_TOUGHNESS, 2).add(Attributes.FOLLOW_RANGE, 20).build();
     public static AttributeSupplier SWAMP_WANDERER = register(35, 8).add(Attributes.KNOCKBACK_RESISTANCE, Tmp.rnd.nextDouble() * 0.05F).add(Attributes.SPAWN_REINFORCEMENTS_CHANCE, Tmp.rnd.nextDouble() * 0.25D + 0.5D).build();
-    public static AttributeSupplier SCOURGE = register(50, 10, 0.15).add(Attributes.KNOCKBACK_RESISTANCE, new Random().nextDouble() * 0.05F).add(Attributes.SPAWN_REINFORCEMENTS_CHANCE, Tmp.rnd.nextDouble() * 0.25D + 0.5D).build();
+    public static AttributeSupplier SCOURGE = register(45, 6, 0.15).add(Attributes.KNOCKBACK_RESISTANCE, new Random().nextDouble() * 0.05F).add(Attributes.SPAWN_REINFORCEMENTS_CHANCE, Tmp.rnd.nextDouble() * 0.25D + 0.5D).build();
     public static AttributeSupplier ENT = register(40, 12, 0.15).add(Attributes.ARMOR, 5).add(Attributes.ARMOR_TOUGHNESS, 2).build();
-    public static AttributeSupplier SORCERER = register(15, 2.5).build();
+    public static AttributeSupplier SORCERER = register(25, 1.5).build();
     public static AttributeSupplier MAGGOT = register(8, 3.5).build();
 
     //minions - overworld
